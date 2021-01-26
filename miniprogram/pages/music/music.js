711746d27585e868225d9b8f4ba6a7b66ee3224e
goufan <1400695792@qq.com>
2021-01-26 19:40:51 +0800

    场景歌单

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -5,8 +5,73 @@ Page({
    * 页面的初始数据
    */
   data: {
+    imgUrls:[
+      {
+        url:'http://p1.music.126.net/pOXTFta-mhTpZOGhBBWvhQ==/109951165664682857.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/C9I9GxpvRX7nCZyXNBeqOw==/109951165664694558.jpg?imageView&quality=89'
+      },
+      {
+        url:'http://p1.music.126.net/q5rKcBx9Y0V37DsUSaQKXg==/109951165664695730.jpg?imageView&quality=89'
+       },
+       {
+        url:'http://p1.music.126.net/WOoIZuva_umxxzYOvWINLA==/109951165664707565.jpg?imageView&quality=89'
+       },
+       {
+        url:'http://p1.music.126.net/j0gp3gBDRRoqIXxAs0v7oA==/109951165664720877.jpg?imageView&quality=89'
+       },
+       {
+        url:'http://p1.music.126.net/9Ayx-EeCnuLRWKTcIhGB6g==/109951165664742856.jpg?imageView&quality=89'
+       },
+       {
+        url:'http://p1.music.126.net/Z90NF2dHuBYrV6x-U9jJJQ==/109951165664719544.jpg?imageView&quality=89'
+       },
+       {
+        url:'http://p1.music.126.net/zUv2mRobckK7Tdn2bp9iSA==/109951165664840470.jpg?imageView&quality=89'
+       }
+    ],
+    playlist:[
+      {
+        "id":"1001",
+        "playCount":"1562万",
+        "name":"[Future Bass] 坠入激流 等待电音洗礼",
+        "picUrl":"http://p4.music.126.net/8RLO4KlE7wnAxbAortkbVw==/109951165543542256.jpg?param=140y140"
+      },
+      {
+        "id":"1002",
+        "playCount":"45.5万",
+        "name":"精选｜Cover翻唱百听不腻",
+        "picUrl":"http://p3.music.126.net/wkb9QH7yBjRMB-DIKadArA==/109951164301459166.jpg?param=140y140"
+      },
+      {
+        "id":"1003",
+        "playCount":"3345",
+        "name":"[欧美私人订制] 最懂你的欧美推荐 每日更新35首",
+        "picUrl":"http://p4.music.126.net/qZOhqXNepqsR8jcjfyTIiA==/109951165545384753.jpg?param=140y140"
+      },
+      {
+        "id":"1004",
+        "playCount":"45.2万",
+        "name":"[聚精会神] 学习工作必备的背景音乐",
+        "picUrl":"http://p3.music.126.net/G-jS2dE3XbqvWFaSk-qU3Q==/109951165545348909.jpg?param=140y140"
+      },
+      {
+        "id":"1005",
+        "playCount":"5545万",
+        "name":"[一周欧美上新] 碧梨携手ROSALÍA献唱《亢奋》原声",
+        "picUrl":"http://p3.music.126.net/iLEV3KhqQPEyATZzkdxMyg==/109951165649686740.jpg?param=140y140"
+      },
+      {
+        "id":"1006",
+        "playCount":"2210",
+        "name":"纯音林涧丨自然 · 音乐",
+        "picUrl":"http://p4.music.126.net/mc7kb-lQGTMSy5zahJrMOQ==/109951165313898113.jpg?param=140y140"
+      }
+    ]
 
   },
+
 
   /**
    * 生命周期函数--监听页面加载
