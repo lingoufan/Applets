8344bdb9839790f739c39781f47bae9a03d01ec0
goufan <1400695792@qq.com>
2021-02-06 22:45:18 +0800

    自定义弹框组件+授权登录组件

---
+++ miniprogram/components/login/login.js
@@ -0,0 +1,23 @@
+Component({
+  properties: {
+    modalShow: Boolean
+  },
+  data: {
+
+  },
+  methods: {
+    onGotUserInfo(event) {
+      console.log(event)
+      const userInfo = event.detail.userInfo
+      //允许授权
+      if (userInfo) {
+        this.setData({
+          modalShow: false
+        })
+        this.triggerEvent('loginsuccess', userInfo)
+      } else {
+        this.triggerEvent('loginfail')
+      }
+    }
+  }
+})
--- miniprogram/pages/find/find.js
+++ miniprogram/pages/find/find.js
@@ -2,15 +2,96 @@ let keyword = ''
 
 Page({
   data: {
+     //控制弹出层是否显示
+     modalShow: false,
   },
   onSearch(event){
     keyword = event.detail.keyword
     console.log(keyword)
   },
   onPublish(){
-    wx.navigateTo({
-      url: '../publish/publish',
-    })
-  },
-   onLoad() {}
+  //获取用户的当前设置。返回值中只会出现小程序已经向用户请求过的权限，根据是否具有scop.userInfo属性，判断用户是否授权
+  wx.getSetting({
+    success: (res) => {
+      console.log('当前设置' + JSON.stringify(res))
+      if (res.authSetting['scope.userInfo']) {
+        wx.getUserInfo({
+          success: (res) => {
+            console.log(res)
+            this.onLoginSuccess({
+              detail: res.userInfo
+            })
+          }
+        })
+      } else {
+        this.setData({
+          modalShow: true,
+        })
+      }
+    }
+  })   
+},
+onLoginSuccess(event) {
+  console.log('>>>>>' + event)
+  const detail = event.detail
+  console.log(detail)
+  wx.navigateTo({
+    url: '../publish/publish',
+  })
+},
+onLoginFail() {
+  wx.showModal({
+    title: '授权用户可发布',
+    content: ''
+  })
+},
+
+/**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+onReady: function () {
+
+},
+
+/**
+ * 生命周期函数--监听页面显示
+ */
+onShow: function () {
+
+},
+
+/**
+ * 生命周期函数--监听页面隐藏
+ */
+onHide: function () {
+
+},
+
+/**
+ * 生命周期函数--监听页面卸载
+ */
+onUnload: function () {
+
+},
+
+/**
+ * 页面相关事件处理函数--监听用户下拉动作
+ */
+onPullDownRefresh: function () {
+
+},
+
+/**
+ * 页面上拉触底事件的处理函数
+ */
+onReachBottom: function () {
+
+},
+
+/**
+ * 用户点击右上角分享
+ */
+onShareAppMessage: function () {
+
+}
 })
